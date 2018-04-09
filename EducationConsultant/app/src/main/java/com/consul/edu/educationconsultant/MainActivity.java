18290e80ed89b266641e8b44a192f945786a3f38
Milena Lalic <mlalic994@gmail.com>
2018-04-10 00:03:56 +0200

    Added logout. #3

--- EducationConsultant/app/src/main/java/com/consul/edu/educationconsultant/MainActivity.java
+++ EducationConsultant/app/src/main/java/com/consul/edu/educationconsultant/MainActivity.java
@@ -1,14 +1,46 @@
 package com.consul.edu.educationconsultant;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends Activity {
+
+    private Button btnLogout;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        btnLogout = (Button) findViewById(R.id.btn_logout);
+        if (btnLogout != null) {
+            btnLogout.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    auth.signOut();
+
+                    FirebaseUser user = auth.getCurrentUser();
+                    if (user == null) {
+                        // user auth state is changed - user is null
+                        // launch login activity
+                        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                        finish();
+                    }
+                }
+            });
+        }
+
+
     }
 }
