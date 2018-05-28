f2e4a136f2df32926e836420f3c2600c6f91fbc5
danilozeka93@gmail.com <tamarazeka95@gmail.com>
2018-05-28 20:19:18 +0200

    sending new question to backend

--- EducationConsultant/app/src/main/java/com/consul/edu/educationconsultant/retrofit/RedditAPI.java
+++ EducationConsultant/app/src/main/java/com/consul/edu/educationconsultant/retrofit/RedditAPI.java
@@ -5,8 +5,13 @@ import com.consul.edu.educationconsultant.model.Question;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 /**
  * Created by Svetlana on 5/16/2018.
@@ -14,11 +19,35 @@ import retrofit2.http.Headers;
 
 public interface RedditAPI {
     // call ipconfig
-    String BASE_URL = "http://192.168.0.14:8095/educon/";
+    String BASE_URL = "http://192.168.0.12:8095/educon/";
 
     @Headers("Content-Type: application/json")
     @GET("question")
     Call<List<Question>> getData();
+
+    @Headers("Content-Type: application/json")
+    @GET("question")
+    Call<List<Question>> getQuestions();
+
+    @Headers("Content-Type: application/json")
+    @GET("question/{id}")
+    Call<Question> getQuestion(@Path("id") Long id);
+
+    @Headers("Content-Type: application/json")
+    @PUT("question/eduLevels")
+    Call<List<Question>> findByEduLevel(); // TODO
+
+    @Headers("Content-Type: application/json")
+    @PUT("question/categories")
+    Call<List<Question>> findByCategories(); // TODO
+
+    @Headers("Content-Type: application/json")
+    @POST("question")
+    Call<Question> insertQuestion(@Body Question question);
+
+    @Headers("Content-Type: application/json")
+    @PUT("question/{id}")
+    Call<Question> updateQuestion(@Path("id") Long id, @Body Question question);
 
 }
 
