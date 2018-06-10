646dfbcd6b1a6f0f129b67abe6c39a90cce08b3d
SvetlanaDjuric <cdjuric963@gmail.com>
2018-06-10 19:43:51 +0200

    backend for notification.

--- EducationConsultant/app/src/main/java/com/consul/edu/educationconsultant/retrofit/RedditAPI.java
+++ EducationConsultant/app/src/main/java/com/consul/edu/educationconsultant/retrofit/RedditAPI.java
@@ -25,7 +25,7 @@ import retrofit2.http.Path;
 
 public interface RedditAPI {
     // call ipconfig
-    String BASE_URL = "http://192.168.0.13:8095/educon/";
+    String BASE_URL = "http://192.168.1.4:8095/educon/";
 
 
     @Headers("Content-Type: application/json")
